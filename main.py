ef77cfa757315aeae2131f28fd348ef2ac9e04d2
Aliasgharttt <jddn4019@gmail.com>
2026-06-19 18:15:15 +0330

    Update main.py

--- main.py
+++ main.py
@@ -559,4 +559,5 @@ async def main():
     logger.info("✅ Bot ready!")
     await dp.start_polling(bot, allowed_updates=["message", "channel_post", "callback_query"])
 
-if __name__ == "__main__"
+if __name__ == "__main__":
+    asyncio.run(main())"
